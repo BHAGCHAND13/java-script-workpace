9265644bf56f064b987de14ea578960770a11600
BHAGCHAND13 <bhagchanddhavale@gmail.com>
2025-02-04 14:53:20 +0000

    done

--- 01.js
+++ 01.js
@@ -1,1 +1,26 @@
 console.log("bhagchand");
+const studentId = 12345;
+let studentName = "bhagchand";
+var studentEmail = "bhagchand@gmail.com";
+student = "male/female";
+let sureName;
+
+
+studentEmail = "dhavale@gamil.com";
+// studentId = 28; - becouse it is a constant value thereof studentId is not update
+studentName = "dhavale";
+student = "male";
+
+/* 
+prefer not to use var,
+becouse is issue in block scope and functional scope 
+*/
+console.log(studentId);
+
+/*
+console.table();
+console.table is second method in js it is use multiple data show on one 
+console windows,
+*/
+console.table([studentId,studentName,studentEmail,student,sureName]);
+
