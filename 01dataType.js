0e07c4d42863945e61c1b76ebb0acb3963019bb2
BHAGCHAND13 <bhagchanddhavale@gmail.com>
2025-02-15 14:23:40 +0000

    variable declar and variable data type

--- 01dataType.js
+++ 01dataType.js
@@ -7,5 +7,17 @@
 // undefind
 // symbol
 
-let score = 76;
-console.log(score);
+let numberTypeVariable = 76;
+let stringTypeVariable = "bhagchand";
+let booleanTypeVariable = true;
+let undefindTypeVariable;
+let nullTypeVariable = null;
+
+console.log(numberTypeVariable,"type of =>",typeof(numberTypeVariable));
+console.log(stringTypeVariable,"type of =>",typeof(stringTypeVariable));
+console.log(booleanTypeVariable,"type of =>",typeof(booleanTypeVariable));
+console.log(undefindTypeVariable,"type of =>",typeof(undefindTypeVariable));
+console.log(nullTypeVariable,"type of =>",typeof(nullTypeVariable));
+
+
+
