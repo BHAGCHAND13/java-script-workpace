a63f11acdb0125f3a269a839dad8b0d0f61b97a7
BHAGCHAND13 <bhagchanddhavale@gmail.com>
2025-02-15 16:26:55 +0000

    data tyoe converting

---
+++ 01dataTypeConversion.js
@@ -0,0 +1,41 @@
+console.log("Number to conversion string and boolean => ")
+let score = 13; // 'score' is mt so check conversion
+console.log(score , typeof(score));
+
+// number to convert string 
+
+let convert = String(score);
+console.log(convert , typeof(convert));
+
+let Convert = Boolean(score);
+console.log(Convert , typeof(Convert));
+
+console.log("Stering conversion to boolean & number =>");
+
+
+// string conversion to boolean & number 
+
+let logIn = "bhagchand"; //MT 'string' to check conversion 
+console.log(logIn,typeof(logIn));
+
+let logInConvert = Boolean(logIn);
+console.log(logInConvert,typeof(logInConvert));
+
+let LogINConvert = Number(logIn);
+console.log(LogINConvert,typeof(LogINConvert));
+
+
+
+console.log("Boolean conversion to number & string =>");
+
+//  Boolean conversion to number & string 
+
+let boolIn = false; // check the 'true' to conversion 
+console.log(boolIn,typeof(boolIn));
+
+let boolInConvert = Number(boolIn);
+console.log(boolInConvert,typeof(boolInConvert));
+
+let BoolInConvert = String(boolIn);
+console.log(BoolInConvert,typeof(BoolInConvert));
+
