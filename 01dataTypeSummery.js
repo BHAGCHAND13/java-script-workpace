0119769c978a7388eb61bb5bb04c9087bd2ec3c5
BHAGCHAND13 <bhagchanddhavale@gmail.com>
2025-02-19 09:31:10 +0000

    summery

---
+++ 01dataTypeSummery.js
@@ -0,0 +1,39 @@
+// there are 2 type of data type...
+// (1) : primitive,
+// (2) : Non-primitive,
+
+// primitive data Type...
+
+// 7 number , BigInt , string , boolean , null , undefind , symbol.
+
+const score = 100;
+const logIn = "bhagchand";
+const logOut = true;
+const value = null;
+let userEmail;  // ex- userEmail = undefind;
+const valueBigInt = 12345678765445n;
+const id = Symbol(123);
+
+// console.log(typeof(id));
+
+
+// refarance Type (Non primitive)...
+
+// array , object , function.
+
+const myArray = [12,34,56,78,90,100];
+// console.log(myArray);
+
+const myObject = {
+    name: "bhagchand",
+    age: 19,
+    qualification: "softwere devoloper",
+};
+// console.log(typeof(myObject));
+
+function myFunction(){
+    console.log("it's my functuon what are work in me");
+}
+// console.log(typeof(myFunction));
+
+
