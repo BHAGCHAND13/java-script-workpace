b1594fed06bbcad454162c1cd085e9bc4c5f9cd2
BHAGCHAND13 <bhagchanddhavale@gmail.com>
2025-02-27 11:47:31 +0000

    done

---
+++ 01dateTime.js
@@ -0,0 +1,18 @@
+// date..
+
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+let newDate = new Date();
+
+console.log(newDate.getDate(),"<= date");
+console.log(newDate.getDay(),"<= day");
+console.log(newDate.getMonth() + 1,"<= month");
+console.log(newDate.getMinutes(),"<= minutes");
+console.log(newDate.getSeconds(),"seconds");
+
