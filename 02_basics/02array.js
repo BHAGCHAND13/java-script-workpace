6a4dcefd71f2b41ec490b0b09d06c47ce8b69ba7
BHAGCHAND13 <bhagchanddhavale@gmail.com>
2025-02-28 17:30:31 +0000

    array  methods done

---
+++ 02_basics/02array.js
@@ -0,0 +1,23 @@
+
+const name = ["bhagchand","pranav","chaitany"];
+const broes = ["sachin","swapnil","rahul","abhisekha"];
+// name.push(broes);
+// console.log(name);
+
+// const allName = name.concat(broes);
+// console.log(allName);
+
+// const newName = [...broes,...name];
+// console.log(newName);
+
+
+const deftArray = [1,2,[3,4,5],6,7,[8,9,[10,11,12]]];
+// console.log(deftArray);
+
+// const oneArray = deftArray.flat(Infinity);
+// console.log(oneArray);
+
+ 
+console.log(Array.isArray(["bhagchand"]));
+console.log(Array.from("bhagchand"));
+console.log(Array.of("bhagchand","madhukar","dhavale"));
