cb16bfb4defaeedfef76905d1edd88d9e405f86d
BHAGCHAND13 <bhagchanddhavale@gmail.com>
2025-03-08 05:31:53 +0000

    done

---
+++ 05_iteration/one.js
@@ -0,0 +1,58 @@
+// loops
+// for loop.....,
+
+// sentax..,
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let a = 1; a <= 10; a++) {
+//     if(a==5){
+//         console.log("detected 5");
+//        break;
+//     }
+//     console.log(a);
+// }
+
+
+// for (let b = 1; b <= 10; b++) {
+//     console.log(b);
+//     if( b==5){
+//         console.log("detected 5");
+//        break;
+//     }
+// }
+
+// for (let c = 1; c <= 10; c++) {
+//     if(b==5){
+//         console.log("detected 5");
+//        continue;
+//     }
+//     console.log(c);
+// }
+
+
+// for (let d = 1; d <= 10; d++) {
+//     console.log(d);
+//     if(d==5){
+//         console.log("detected 5");
+//        continue;
+//     }
+// }
+
+// for(let i = 1 ; i<=10 ; i++){
+//     console.log(` teble ${i}th`);
+//     for(let j = 1 ; j<= 10 ; j++){
+//         console.log(i ,"*",j,"=",i*j);
+//     }
+// }
+
+let myarray = ["flash","batman","superman","spiderman"];
+console.log(myarray.length);
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    console.log(element);
+    
+}
---
+++ 05_iteration/two.js
@@ -0,0 +1,35 @@
+// while loop....,
+
+let value = 2;
+while(value <= 20){
+    // console.log(`value of index is ${value}`);
+    value = value+1;
+}
+
+let number = 2;
+while(number <= 20){
+    // console.log(`number of index is ${number}`);
+    number = number+2;
+}
+
+const fivePandavName = ["Yudhishthira","Bhima","Arjuna","Nakula","Sahadeva"];
+let val = 0;
+while(val < fivePandavName.length){
+    // console.log(`value is ${val} : ${fivePandavName[val]}`);
+    val++;
+}
+
+// do while loop....,
+
+// let Index = 1;
+// do{
+//     console.log(`index is ${Index}`);
+//     Index++;
+// }while(Index<=10);
+
+
+// let table = 2;
+// do{
+//     console.log(`index is ${table}`);
+//     table = table +2;
+// }while(table<=20);
