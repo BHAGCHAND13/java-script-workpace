541a9b88a68bb23be31e9328af5c98379ab2ae68
BHAGCHAND13 <bhagchanddhavale@gmail.com>
2025-03-15 14:23:43 +0530

    Add files via upload

---
+++ 05_iteration/seven.js
@@ -0,0 +1,32 @@
+const stuInfo = [
+    {name:"bhagcgand",age:19,sureName:"dhavale"},
+    {name:"abhishek",age:18,sureName:"dhavale"},
+    {name:"omkar",age:18,sureName:"gavande"},
+    {name:"ganesh",age:40,sureName:"pawar"},
+    {name:"hari",age:21,sureName:"ugalmugle"},
+    {name:"suraj",age:20,sureName:"bhusare"},
+    {name:"ganesh",age:28,sureName:"dhavale"},
+    {name:"datta",age:20,sureName:"panhale"},
+    {name:"sunil",age:23,sureName:"aadhe"},
+    {name:"narendra",age:17,sureName:"giramkar"},
+]
+// NAme = stuInfo.filter((n)=>{
+//     return n.age < 18;
+// })
+// console.log(NAme);
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(myNumbers);
+
+
+// newNumbers =  myNumbers.map((num)=> num * 10);
+// console.log(newNumbers);
+
+// const newNum = myNumbers.map((num)=> num*10)
+//                         .map((num)=> num+2)
+//                         .filter((num)=> num>41)
+//                         console.log(newNum);
+                        
+
+
